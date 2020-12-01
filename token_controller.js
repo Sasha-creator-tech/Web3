4dfba0e36d1f1e06a87fc01683b82213d7850708
Sasha-creator-tech <71944607+Sasha-creator-tech@users.noreply.github.com>
2020-12-01 12:42:23 +0200

    DudleTokenWeb3
    Get balance of

--- token_controller.js
+++ token_controller.js
@@ -49,10 +49,24 @@ const miniByOwner = async(destAddresses, transferAmount, getBalanceOf) => {
     console.log('Balance of address :' + getBalanceOf + ' is: ' + balance);
 };
 
-miniByOwner([
-	//add amount of addresses here as much as you like
-	'0x0b4104c9c4bf34f82a84C19B06A6dF75B9637781'],
-	//add here amount of token to mint it on input addresses
-	 (3 * 1e9),
-	 //add here address you want to see balance of
-	'0x0b4104c9c4bf34f82a84C19B06A6dF75B9637781');
+// miniByOwner([
+// 	//add amount of addresses here as much as you like
+// 	'0x0b4104c9c4bf34f82a84C19B06A6dF75B9637781'],
+// 	//add here amount of token to mint it on input addresses
+// 	 (3 * 1e9),
+// 	 //add here address you want to see balance of
+// 	'0x0b4104c9c4bf34f82a84C19B06A6dF75B9637781');
+
+const getBalanceOf = async(abiContr, contractAddress, holderAddress) => {
+	var contract = new web3.eth.Contract(JSON.parse(abiContr), contractAddress);
+	var balance = await contract.methods.balanceOf(holderAddress).call();
+    console.log('Balance of address :' + holderAddress + ' is: ' + balance);
+};
+
+getBalanceOf(
+	//put here abi of the contract
+	'[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]',
+	//put here address of the contract
+	'0xE1E545c89F3996Cf06E1f12C6D05e3412c7C7b11',
+	//put here address of the holder to get balance of
+ 	'0x635a615769f044eaba81f7ae9797d9901a5b09e5');
